b3536c8a0606514fe3bc822311784ee647631ea9
Veg2281337 <romanov.vd07@gmail.com>
2022-06-09 16:16:48 +0300

    ...

--- task 1/строка в строке.pas
+++ task 1/строка в строке.pas
@@ -1,14 +1,18 @@
 ﻿  function searching_for_a_string_within_string(s:string; f:string):integer;
   begin
     var t, d: integer;
-    setlength(s, t);
-    for var i:=0 to t do 
+    t:=length(s);
+    d:=length(f);
+    for var i:=1 to t do 
     begin
-      if s[i] = f[1] then searching_for_a_string_within_string:=i
+      if s[i] = f[1] then 
+      begin
+        searching_for_a_string_within_string:=i-1;
+        break
+      end
       else searching_for_a_string_within_string:= -1
     end
   end;
-  // объясни, почему он игнорирует условие
   
   begin
     var s, f: string;
