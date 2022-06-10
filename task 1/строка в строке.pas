32dd6d31b18fa2d89b907d8c4629d12040dd69ce
Veg2281337 <romanov.vd07@gmail.com>
2022-06-10 11:03:25 +0300

    ...

--- task 1/строка в строке.pas
+++ task 1/строка в строке.pas
@@ -18,9 +18,30 @@
     if  c<>d then searching_for_a_string_within_string:= -1
   end;
   
+  function gg(s:string; f:string):integer;
+  begin
+    var t, d: integer;
+    t:=length(s);
+    d:=length(f);
+    var str : string;
+    for var i:=1 to t do
+    begin
+      delete(str, 1, d);
+      for var j:=1 to d do str:=str + s[j+i-1];
+      if str = f then 
+      begin
+        gg:=i-1;
+        break
+      end
+      else gg:=-1;
+      if i = (t-(t mod d)) then break;
+    end;
+  end;
+  
   begin
     var s, f: string;
     readln(s);
     readln(f);
-    writeln(searching_for_a_string_within_string(s, f))
+    writeln(searching_for_a_string_within_string(s, f));
+    writeln(gg(s, f))
   end.
